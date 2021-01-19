21ace19b19bb55532f0d377a009d78b0db224af3
Paulo Azoia <pazoia.gomes@gmail.com>
2021-01-19 17:39:47 +0000

    Skip buttons, volume and playback speed, progress bar all working

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-// Get our Elements
+/* Get our Elements */
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
@@ -7,7 +7,7 @@ const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
 
-// Build our functions
+/* Build our functions */
 function togglePlay() {
     if (video.paused) {
         video.play();
@@ -21,10 +21,29 @@ function updateButton() {
     toggle.textContent = icon;
 }
 
-// Hook up the event listners
+function skip() {
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+    video[this.name] = this.value;
+}
+
+function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
+
+/* Hook up the event listners */
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
-
+video.addEventListener('timeupdate', handleProgress);
 
 toggle.addEventListener('click', togglePlay);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
+
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
